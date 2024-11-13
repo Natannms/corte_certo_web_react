a17dc1b584bd746a7023ba5f7014932c9040f871
Natannms <agnusnat.nms@gmail.com>
2024-11-13 20:08:28 -0300

    Feat: Adiciona possibilidade de convite de colcaborador e configuração de unidade

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from 'react';
 import bg from '../assets/backgrounds/signin.jpg';
-import { AuthResponse, login } from '../api/api'; // Assumindo que a função de login foi criada no arquivo `api.ts`
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 import Lottie from "lottie-react";
 import loadingLottie from "../assets/lottie/loading.json";
 import { useUserStore } from '../contexts/useUserStore';
 import { toast } from 'react-toastify';
+import { AuthResponse, login } from '../../src/api/api';
+
 const LoginScreen = () => {
     const navigate: NavigateFunction = useNavigate();
-    const {setName, setToken} = useUserStore()
+    const { setName, setToken } = useUserStore();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleLogin = async () => {
-        setError(''); 
+        setIsLoading(true); // Define isLoading diretamente como true
+        setError('');
+
         const response: AuthResponse = await login({ email, password });
 
         if (response.error) {
@@ -23,34 +26,34 @@ const LoginScreen = () => {
             toast(response.error);
         } else {
             const { user, token } = response;
-            if(token && user){
+            if (token && user) {
                 setName(user.name)
                 setToken(token)
+                setIsLoading(false)
                 navigate('/dashboard', { replace: true, state: { user } })
                 return;
             }
-            
+
             setError("Não foi possivel realiar login, dados não foram obtidos corretamente!");
             toast("Não foi possivel realiar login, dados não foram obtidos corretamente!");
 
         }
+        setIsLoading(false);
     };
 
-    async function loading(){
-        const token =localStorage.getItem('token')
-        if(token && token !== ''){
-            setTimeout(() => {
-                setIsLoading(false);
-            }, 1000);
-            navigate('/dashboard', {replace: true})
+    async function loading() {
+        const token = localStorage.getItem('token');
+        if (token && token !== '') {
+            navigate('/dashboard', { replace: true });
             return;
         }
         setIsLoading(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         loading();
-    })
+    }, []);
+
     return (
         <div className='bg-stone-800 text-white w-full h-screen flex flex-row'>
             <div
@@ -63,12 +66,10 @@ const LoginScreen = () => {
             ></div>
             <div className='bg-stone-800 p-8 w-full flex flex-col justify-center items-center'>
                 <h1 className='text-4xl font-bold mb-8'>Barber Shop App</h1>
-                {isLoading ?
-                    <Lottie animationData={loadingLottie} />
-                    :
+              
                     <div className='w-8/12'>
                         {error && <div className="bg-red-600 p-2 rounded mb-4">{error}</div>}
-                        
+
                         <input
                             type='email'
                             placeholder='Email'
@@ -83,20 +84,26 @@ const LoginScreen = () => {
                             onChange={(e) => setPassword(e.target.value)}
                             className='w-full p-2 mb-4 bg-stone-700 text-white rounded'
                         />
+
                         <button
                             onClick={handleLogin}
-                            className='bg-yellow-500 text-white w-full p-2 rounded hover:bg-yellow-600'
+                            className={`flex items-center justify-center text-white w-full p-2 rounded ${isLoading ? 'bg-gray-500' : 'bg-yellow-500 hover:bg-yellow-600'}`}
+                            disabled={isLoading}
                         >
-                            Sign In
+                            {isLoading ? (
+                                 <Lottie animationData={loadingLottie} className='w-8'/>
+                            ) : (
+                                'Sign In'
+                            )}
                         </button>
+
                         <p className='mt-4 text-sm'>
                             Não tem uma conta? <a href='/register' className='text-yellow-400 hover:underline'>Registre-se</a>
                         </p>
                     </div>
-                }
             </div>
         </div>
     );
-}
+};
 
 export default LoginScreen;
---
+++ src/components/BarberShopConfigForm.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { updateBarberShop } from '../api/api'; // Função de API fictícia para atualizar os dados da barbearia
+import { useUserStore, useBarberShopStore } from '../contexts';
+import { toast } from 'react-toastify';
+import { BarberShop } from 'src/types/BarberShop';
+// import { X } from '@phosphor-icons/react';
+type Props = {
+    updateBarberShopsList: ()=>void
+}
+const BarberShopConfigForm = ({updateBarberShopsList}:Props) => {
+    const [name, setName] = useState<string>('');
+    const [address, setAddress] = useState<string>('');
+    const [startWork, setStartWork] = useState<string>('');
+    const [endWork, setEndWork] = useState<string>('');
+
+    const { token } = useUserStore();
+    const { selectedBarberShop, barberShops } = useBarberShopStore();
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const barberShopData = {
+            name,
+            address,
+            startWork,
+            endWork,
+        };
+
+        // Enviar os dados para o servidor
+        try {
+            const response = await updateBarberShop(token, selectedBarberShop, barberShopData);
+            if (response.success) {
+                updateBarberShopsList()
+               toast("Atualizado com sucesso", {type:"success"})
+            }
+            toast(response.success, { type: "success" });
+        } catch (error) {
+            toast('Erro ao atualizar informações da barbearia: ' + error, { type: 'error' });
+            console.error('Erro ao atualizar informações da barbearia:', error);
+        }
+    };
+
+
+
+    return (
+        <form onSubmit={handleSubmit} className="p-6 flex flex-col gap-8 rounded items-center w-full">
+            <div className="flex flex-col w-full  items-center">
+                <label htmlFor="name">Nome da Barbearia : </label>
+                <input
+                    className="input"
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    
+                    placeholder={barberShops.filter((item: BarberShop) => item.id === selectedBarberShop)[0].name}
+                />
+            </div>
+            <div className="flex flex-col w-full  items-center">
+                <label htmlFor="address">Endereço:</label>
+                <input
+                    placeholder={barberShops.filter((item: BarberShop) => item.id === selectedBarberShop)[0].address}
+                    className="input"
+                    type="text"
+                    id="address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    
+                />
+                <small></small>
+            </div>
+            <div className="flex flex-col w-full  items-center">
+                <label htmlFor="startWork">Horário de Início:</label>
+                <input
+                    className="input"
+                    type="time"
+                    id="startWork"
+                    value={startWork}
+                    onChange={(e) => setStartWork(e.target.value)}
+                    
+                />
+                <small className="">{barberShops.filter((item: BarberShop) => item.id === selectedBarberShop)[0].startWork? barberShops.filter((item: BarberShop) => item.id === selectedBarberShop)[0].startWork: 'horário de inicio ainda não foi definido'}</small>
+            </div>
+            <div className="flex flex-col w-full  items-center">
+                <label htmlFor="endWork">Horário de Encerramento:</label>
+                <input
+                    className="input"
+                    type="time"
+                    id="endWork"
+                    value={endWork}
+                    onChange={(e) => setEndWork(e.target.value)}
+                    
+                />
+                <small className="">{barberShops.filter((item: BarberShop) => item.id === selectedBarberShop)[0].startWork ? barberShops.filter((item: BarberShop) => item.id === selectedBarberShop)[0].startWork : 'horário de encerramento ainda não foi definido'}</small>
+
+            </div>
+            <div>
+                <button type="submit" className="btn btn-primary w-full">Salvar Configurações</button>
+            </div>
+        </form>
+    );
+};
+
+export default BarberShopConfigForm;
